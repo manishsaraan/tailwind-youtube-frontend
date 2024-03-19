fdd08fd88cb25e9dd5f58c9c9c44425eb996dbe6
manishsaraan <manish021js@gmail.com>
2024-03-19 17:55:23 +0530

    updates in ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { VideoGrids } from '@/components/VideoGrids'
-import { AppBar } from '@/components/AppBar'
+import { VideoGrids } from "@/components/VideoGrids";
+import { AppBar } from "@/components/AppBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
-     <AppBar/>
-        <VideoGrids  />
-    </>
-       
+    <main>
+      <AppBar />
+      <div className="grid  grid-cols-12">
+        <div className="col-span-2 flex justify-center">
+          sidebar
+        </div>
+        <div className="col-span-10 pt-5">
+          <VideoGrids />
+        </div>
+      </div>
+    </main>
   );
 }
